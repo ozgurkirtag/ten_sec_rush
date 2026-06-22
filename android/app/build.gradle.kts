e9cfa5a07386668d47d50208c54b566c9785f22d
ozgurkirtag <ozgurkirtag@gmail.com>
2026-06-22 19:55:03 +0000

    Fix release signing config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,6 +34,15 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
